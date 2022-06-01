b0ffe8ae7c3212beba5af1cbcaf33b9e08b3311b
gezish <enggezahegn.w@gmail.com>
2022-06-01 12:22:07 +0300

    cleaning module

---
+++ scripts/data_cleaning.py
@@ -0,0 +1,25 @@
+"""import your liberaries here"""
+
+
+
+
+class Clean:
+    
+    
+    def __init__(self,df = None):
+        """initialize the cleaning class"""
+        self.df = df
+        print("Successfully initialized clean class")
+    
+    # function to openfile   
+    # function to convert signal to a fixed length 
+    # function to get features from the audio 
+    # function to load audiofile 
+    # function to get lebel of the data 
+    # function to read_text 
+    # function to  data from training and testing file paths 
+    # function to get duration of a file
+    # function to convert_channels to stereo
+    
+    
+         
