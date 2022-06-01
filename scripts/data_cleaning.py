6d043a5c8db3d5b11bbb117c6c7628bad3d28e14
gezish <enggezahegn.w@gmail.com>
2022-06-01 12:53:04 +0300

    file load compete

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,5 +1,22 @@
 """import your liberaries here"""
-
+import imp
+import librosa
+import numpy as np
+import pandas as pd
+import warnings
+import random
+import matplotlib.pyplot as plt
+from os.path import exists
+import seaborn as sns
+from functools import reduce
+import os,sys
+from sklearn.impute import SimpleImputer
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.pipeline import Pipeline
+import torch
+import torchaudio
+import random
 
 
 
@@ -12,6 +29,16 @@ class Clean:
         print("Successfully initialized clean class")
     
     # function to openfile   
+    def openfile(self, audio_file):
+        """
+        - Gezahegne
+        - to open audio file and return the signal and sampling rate
+        """
+        sig, sr = torchaudio.load(audio_file)
+        #print(sig, sr)
+        return (sig, sr)
+    
+    
     # function to convert signal to a fixed length 
     # function to get features from the audio 
     # function to load audiofile 
